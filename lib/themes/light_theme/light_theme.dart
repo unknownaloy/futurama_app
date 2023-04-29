6e6874a9b6c228206bbdfe9edb33ff64b024c26b
unknownaloy <niceju4u@gmail.com>
2023-04-29 17:33:53 +0100

    Added theme data for the BottomNavigationBarThemeData

--- lib/themes/light_theme/light_theme.dart
+++ lib/themes/light_theme/light_theme.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:futurama_app/themes/light_theme/light_text_theme.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 ThemeData lightTheme() {
   return ThemeData.light().copyWith(
@@ -18,11 +17,27 @@ ThemeData lightTheme() {
     ),
     visualDensity: VisualDensity.adaptivePlatformDensity,
     textTheme: kLightTextTheme,
-    iconButtonTheme: IconButtonThemeData(
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(Colors.orangeAccent)
-      )
-    )
+    chipTheme: const ChipThemeData(
+      backgroundColor: Color(0xffF2BC8C),
+    ),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      elevation: 0.0,
+      selectedItemColor: Color(0xff7AC7F0),
+      unselectedItemColor: Colors.grey,
+      backgroundColor: Color(0xffEDF0F5),
+      showSelectedLabels: true,
+      showUnselectedLabels: true,
+      type: BottomNavigationBarType.fixed,
+      selectedIconTheme: IconThemeData(
+        color: Color(0xff7AC7F0),
+      ),
+      unselectedIconTheme: IconThemeData(
+        color: Colors.grey,
+      ),
+    ),
+
+
+
     // iconTheme: const IconThemeData(color: kWhiteColor),
     // textButtonTheme: kTextButtonThemeData(),
     // bottomSheetTheme: kBottomSheetTheme(),
